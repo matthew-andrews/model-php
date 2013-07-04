feb84269a83162cc97a068d490d830d0bfcff3d6
Matthew Andrews <matthew@andrews.eu.com>
2013-07-04 02:07:07 +0100

    Fix bug in toJSON method

--- test/MattAndrews/model.php
+++ test/MattAndrews/model.php
@@ -2,48 +2,54 @@
 
 class ModelTest extends \PHPUnit_Framework_TestCase {
 
-  private $_sampleData = array(
-    'foo' => 'oof',
-    'bar' => 'rab',
-    'baz' => 'zab'
-  );
+    private $_sampleData = array(
+        'foo' => 'oof',
+        'bar' => 'rab',
+        'baz' => 'zab'
+    );
 
-  public function testStoresTheArrayPassedIntoTheConstructor() {
-    $model = new \MattAndrews\Model($this->_sampleData);
+    public function testStoresTheArrayPassedIntoTheConstructor() {
+        $model = new \MattAndrews\Model($this->_sampleData);
 
-    $this->assertEquals('oof', $model->get('foo'));
-    $this->assertEquals('rab', $model->get('bar'));
-    $this->assertEquals('zab', $model->get('baz'));
-  }
-
-  public function testCanBeConstructedWithNoData() {
-    $threwException = false;
-    try {
-      $model = new \MattAndrews\Model();
-    } catch (Exception $exception) {
-      $threwException = true;
+        $this->assertEquals('oof', $model->get('foo'));
+        $this->assertEquals('rab', $model->get('bar'));
+        $this->assertEquals('zab', $model->get('baz'));
     }
-    $this->assertFalse($threwException);
-  }
 
-  public function testCanOverwriteSpecificKey() {
-    $model = new \MattAndrews\Model($this->_sampleData);
-    $model->set('foo', 'foobar');
+    public function testCanBeConstructedWithNoData() {
+        $threwException = false;
+        try {
+            $model = new \MattAndrews\Model();
+        } catch (Exception $exception) {
+            $threwException = true;
+        }
+        $this->assertFalse($threwException);
+    }
 
-    $this->assertEquals('foobar', $model->get('foo'));
-    $this->assertEquals('rab', $model->get('bar'));
-    $this->assertEquals('zab', $model->get('baz'));
-  }
+    public function testCanOverwriteSpecificKey() {
+        $model = new \MattAndrews\Model($this->_sampleData);
+        $model->set('foo', 'foobar');
 
-  public function testCanGetAllData() {
-    $model = new \MattAndrews\Model($this->_sampleData);
-    $this->assertEquals($this->_sampleData, $model->get());
-  }
+        $this->assertEquals('foobar', $model->get('foo'));
+        $this->assertEquals('rab', $model->get('bar'));
+        $this->assertEquals('zab', $model->get('baz'));
+    }
 
-  public function testSetAndClearChain() {
-    $model = new \MattAndrews\Model();
-    $model->set('apples', 'pears')->clear()->set('123', '456');
-    $this->assertEquals(array('123' => '456'), $model->get());
-  }
+    public function testCanGetAllData() {
+        $model = new \MattAndrews\Model($this->_sampleData);
+        $this->assertEquals($this->_sampleData, $model->get());
+    }
 
+    public function testSetAndClearChain() {
+        $model = new \MattAndrews\Model();
+        $model->set('apples', 'pears')->clear()->set('123', '456');
+        $this->assertEquals(array('123' => '456'), $model->get());
+    }
+
+    public function testToJsonReturnsSensibly() {
+        $model = new \MattAndrews\Model();
+        $model->set('apple', 'pear');
+        $actual = $model->toJSON();
+        $this->assertEquals(array('apple' => 'pear'), $actual);
+    }
 }
