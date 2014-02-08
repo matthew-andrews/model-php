31929d1a820b4fa2e6273cbeeda64bf696fd3b92
Matt Andrews <matt@mattandre.ws>
2014-02-08 18:21:05 +0000

    Update model.toJSON.php

--- test/MattAndrews/model.toJSON.php
+++ test/MattAndrews/model.toJSON.php
@@ -2,16 +2,15 @@
 
 class ModelToJSONTest extends \PHPUnit_Framework_TestCase {
 
-  public function testStoresTheArrayPassedIntoTheConstructor() {
+  public function test_toJSON_exports_the_data_in_a_json_serializable_way() {
     $model = new \MattAndrews\Model(array(
       'foo' => 'foo',
       'bar' => 'bar',
       'baz' => 'baz'
     ));
+    $actual = $model->toJSON();
 
-    $this->assertEquals('foo', $model->get('foo'));
-    $this->assertEquals('bar', $model->get('bar'));
-    $this->assertEquals('baz', $model->get('baz'));
+    $this->assertEquals('{"foo":"oof","bar":"rab","baz":"zab"}', json_encode($actual));
   }
 
 }
